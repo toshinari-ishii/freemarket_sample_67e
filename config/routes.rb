77503838371e663087e17e525adf32f4abadaf04
toshinari-ishii <toshi1225m@gmail.com>
2020-02-06 02:41:01 +0900

    クレジット決済機能

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,13 @@ Rails.application.routes.draw do
       get 'confirm'
     end
   end
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
   resources :users, only: [:show]
 end
    
