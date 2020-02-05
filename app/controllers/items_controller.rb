8e45b93bf2c08c69902fef7586d2de832cd1f896
yuichiro-cloud <ghjhbc1@gmail.com>
2020-02-05 14:35:51 +0900

    created-function-of-items-index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.where(buyer: nil).order(id:"DESC")
+    @photos = Photo.all
+  end
   def new
-    
+
   end
   def show
     
