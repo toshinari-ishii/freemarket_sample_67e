e4e37af0f9d15d52fc532e4c0144a53cc613d1cf
toshinari-ishii <toshi1225m@gmail.com>
2020-02-06 18:09:00 +0900

    マイグレーションファイル　削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2020_02_06_065219) do
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "ancestry"
-  end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
-
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -44,12 +34,10 @@ ActiveRecord::Schema.define(version: 2020_02_06_065219) do
     t.string "area", null: false
     t.integer "day", null: false
     t.integer "price", null: false
-    t.bigint "category_id", null: false
     t.bigint "user_id", null: false
     t.integer "buyer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -85,10 +73,7 @@ ActiveRecord::Schema.define(version: 2020_02_06_065219) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "items", "categories"
   add_foreign_key "cards", "users"
-  add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
   add_foreign_key "photos", "items"
 end
