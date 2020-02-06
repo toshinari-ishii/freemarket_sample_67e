e60ce6637aace252b83f370e78577607bf609f6f
souzu-takuya <59201498+souzu-takuya@users.noreply.github.com>
2020-02-06 17:21:16 +0900

    カテゴリー完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   has_many :photos, dependent: :destroy
   belongs_to :user
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_115641) do
+ActiveRecord::Schema.define(version: 2020_02_06_065812) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -27,10 +34,12 @@ ActiveRecord::Schema.define(version: 2020_02_05_115641) do
     t.string "area", null: false
     t.integer "day", null: false
     t.integer "price", null: false
+    t.bigint "category_id", null: false
     t.bigint "user_id", null: false
     t.integer "buyer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -67,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_02_05_115641) do
   end
 
   add_foreign_key "cards", "users"
+  add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
   add_foreign_key "photos", "items"
 end
