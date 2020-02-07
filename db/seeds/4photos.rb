a3af9d2ce9e11171ae758615fbb56097b4a689ee
Kenichi Takada <test@gmail.com>
2020-02-07 12:10:06 +0900

    初期データ

---
+++ db/seeds/4photos.rb
@@ -0,0 +1,42 @@
+Photo.create!(
+  image: "https://cdn.thumb.shop-list.com/res/up/shoplist/shp/__basethum900__/fashionletter/ry01/ry360_s_1.jpg",
+  item_id: 1
+)
+
+Photo.create!(
+  image: "https://image.rakuten.co.jp/idealstore/cabinet/item-e/epsd003.jpg",
+  item_id: 2
+)
+
+Photo.create!(
+  image: "https://d1bdgtbdniw5kz.cloudfront.net/img/goods/L/CF4903111186860.jpg",
+  item_id: 3,
+)
+Photo.create!(
+  image: "https://www.futonten.info/wordpress/wp-content/uploads/2018/05/20180515_pillow.jpg",
+  item_id: 4,
+)
+Photo.create!(
+  image: "https://www.cmoa.jp/data/image/title/title_0000115452/VOLUME/100001154520001.jpg",
+  item_id: 5,
+)
+Photo.create!(
+  image: "https://images-na.ssl-images-amazon.com/images/I/41VJPmAf0UL._SX342_.jpg",
+  item_id: 6,
+)
+Photo.create!(
+  image: "https://images-na.ssl-images-amazon.com/images/I/31QEawYWA5L._AC_.jpg",
+  item_id: 7,
+)
+Photo.create!(
+  image: "https://images-na.ssl-images-amazon.com/images/I/71I3h3QRL7L._AC_SX425_.jpg",
+  item_id: 8,
+)
+Photo.create!(
+  image: "https://images-na.ssl-images-amazon.com/images/I/81aayfNGaIL._SY355_.jpg",
+  item_id: 9,
+)
+Photo.create!(
+  image: "https://cdn.mainichi.jp/vol1/2019/08/23/20190823mpj00m040005000p/8.jpg?1",
+  item_id: 10,
+)
