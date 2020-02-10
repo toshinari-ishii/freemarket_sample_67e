26cabdf344d823094845951928bf6686cf7acc03
souzu-takuya <59201498+souzu-takuya@users.noreply.github.com>
2020-02-10 13:11:23 +0900

    カードマイグレーション変更

--- db/migrate/20200205115641_create_cards.rb
+++ db/migrate/20200205115641_create_cards.rb
@@ -2,6 +2,8 @@ class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
       # t.integer :number
+      t.string :customer_id
+      t.string :card_id
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
