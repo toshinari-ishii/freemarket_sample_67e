1b7e0f505efd760defdcc228014611b4d80a8440
souzu-takuya <59201498+souzu-takuya@users.noreply.github.com>
2020-02-11 18:44:18 +0900

    アソシエーションhas_oneに変更

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -10,7 +10,7 @@ class CardController < ApplicationController
 
 
   def create #PayjpとCardのデータベースを作成
-    Payjp.api_key = "sk_test_265301405b3342f54b10a319"
+    Payjp.api_key = "sk_test_ce027fe0327f98d0ac645919"
 
     if params['payjp-token'].blank?
       redirect_to action: "new"
@@ -40,7 +40,7 @@ class CardController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     if card.blank?
     else
-      Payjp.api_key = "sk_test_265301405b3342f54b10a319"
+      Payjp.api_key = "sk_test_ce027fe0327f98d0ac645919"
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.delete
@@ -58,7 +58,7 @@ class CardController < ApplicationController
     if card.blank?
       redirect_to action: "new" 
     else
-      Payjp.api_key = "sk_test_265301405b3342f54b10a319"
+      Payjp.api_key = "sk_test_ce027fe0327f98d0ac645919"
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
