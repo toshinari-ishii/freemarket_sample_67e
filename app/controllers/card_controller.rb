5b498ca4e395934c5bd4563f511ed00125c90ff3
Kenichi Takada <test@gmail.com>
2020-02-11 12:53:32 +0900

    createTokenの途中1

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -53,10 +53,12 @@ class CardController < ApplicationController
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
+  end
 
   private
 
   def set_card
     @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
   end
+  
 end
