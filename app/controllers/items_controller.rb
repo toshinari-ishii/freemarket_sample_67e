0ac741f09fe0a2d82feb1c5363dfe035fd493474
Kenichi Takada <test@gmail.com>
2020-02-12 14:27:05 +0900

    クレジットカードを利用した支払い＋購入した際、Payjpのコンソールから売上が確認できる＋購入完了したらトップページに戻る

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,17 +25,14 @@ class ItemsController < ApplicationController
   def buy
     @item = Item.find(params[:id])
     @item.update(buyer: current_user.id)
-    @card = Card.find_by(user_id: current_user.id)
-    @buy = @card.customer_id
 
-    Payjp.api_key = "sk_test_ce027fe0327f98d0ac645919"
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: 809, # 決済する値段
-      card: "#{@buy}",
-      # card: params['payjp-token'], フォームを送信すると作成・送信されてくるトークン
+      customer: card.customer_id, # 顧客ID
       currency: 'jpy'
     )
-    
     redirect_to '/'
   end
 
