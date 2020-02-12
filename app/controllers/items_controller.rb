77528d52ad20f1835d7f01c1e630a589ce6b1436
souzu-takuya <59201498+souzu-takuya@users.noreply.github.com>
2020-02-12 11:24:30 +0900

    buyerカラムに値を入れる成功

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,16 @@ class ItemsController < ApplicationController
   def buy
     @item = Item.find(params[:id])
     @item.update(buyer: current_user.id)
+    @card = Card.find_by(user_id: current_user.id)
+    @buy = @card.customer_id
+
+    Payjp.api_key = "sk_test_ce027fe0327f98d0ac645919"
+    Payjp::Charge.create(
+      amount: 809, # 決済する値段
+      card: "#{@buy}",
+      # card: params['payjp-token'], フォームを送信すると作成・送信されてくるトークン
+      currency: 'jpy'
+    )
     redirect_to '/'
   end
 
