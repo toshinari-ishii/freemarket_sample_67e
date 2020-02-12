071d7985a8cff1630ffbafb385cd283b47ae7f76
kaigunkuzan <liverpool81998@gmail.com>
2020-02-12 14:54:41 +0900

    商品情報編集機能2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :items, only:[:new, :show, :destroy,:create] do
+  resources :items, only:[:new, :show, :destroy,:create, :edit] do
     member do
       get 'buy'
       get 'confirm'
