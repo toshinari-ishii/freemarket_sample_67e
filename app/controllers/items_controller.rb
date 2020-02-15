6a2fa0ad9f9440db3d856a621abc65b176a32c62
yuichiro-cloud <ghjhbc1@gmail.com>
2020-02-15 12:03:33 +0900

    編集機能修正済み

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -89,11 +89,12 @@ class ItemsController < ApplicationController
     @parent = @child.parent
     @children = @child.siblings
     @parents = @parent.siblings
-    @photos = Photo.where(item_id: params[:id])
+    # @photos = Photo.where(item_id: params[:id])
 
   end
 
   def update
+    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -103,7 +104,7 @@ class ItemsController < ApplicationController
   
   private
   def item_params
-    params.require(:item).permit(:name, :text,:condition,:burden, :area, :day, :price, :category_id, :user_id, :buyer , photos_attributes: [:image]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :text,:condition,:burden, :area, :day, :price, :category_id, :user_id, :buyer , photos_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
   end
 
   
