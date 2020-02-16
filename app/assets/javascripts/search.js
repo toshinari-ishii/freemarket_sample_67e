567e5b8ca1953057d8b295b83c8ba9438c981a2e
Kenichi Takada <test@gmail.com>
2020-02-16 11:22:05 +0900

    position: absolute;でbackground imageの上に表示

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -23,4 +23,33 @@ $(function() {
       alert("NG");
     });
   });
+
+  // 子カテゴリーを追加するための処理です。
+  function buildChildHTML(child){
+    var html =`<a class="child_category" id="${child.id}" 
+                href="/category/${child.id}">${child.name}</a>`;
+    return html;
+  }
+
+  $(".parent_category").on("mouseover", function() {
+    var id = this.id//どのリンクにマウスが乗ってるのか取得します
+    $(".now-selected-red").removeClass("now-selected-red")//赤色のcssのためです
+    $('#' + id).addClass("now-selected-red");//赤色のcssのためです
+    $(".child_category").remove();//一旦出ている子カテゴリ消します！
+    $(".grand_child_category").remove();//孫、てめえもだ！
+    $.ajax({
+      type: 'GET',
+      url: '/category/new',//とりあえずここでは、newアクションに飛ばしてます
+      data: {parent_id: id},//どの親の要素かを送ります　params[:parent_id]で送られる
+      dataType: 'json'
+    })
+    .done(function(children) {
+      children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
+        var html = buildChildHTML(child);//HTMLにして
+        $(".children_list").append(html);//リストに追加します
+      })
+    });
+  });
+
+
 });
