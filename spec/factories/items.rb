890ed7df646f20b2e81b1fc17dbc16715c6c4d73
kaigunkuzan <liverpool81998@gmail.com>
2020-02-17 14:05:55 +0900

    出品機能バリーデーション完了

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
+    name          {"バナナ"}
+    text          {"美味しい"}
+    condition     {"まだ黄色です"}
+    burden        {"送料込み(出品者負担"}
+    area          {"北海道"}
+    day           {"1~2日で発送"}
+    price         {"7000"}
     
   end
 end
