2a9a9239ef0577caf31e9e3a12b6156c8945d639
toshinari-ishii <toshi1225m@gmail.com>
2020-02-18 13:18:05 +0900

    

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
 
-    config.fog_directory  = 'freemarketimage'
+    config.fog_directory  = 'freemarketiamge'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketimage'
   end
 end
