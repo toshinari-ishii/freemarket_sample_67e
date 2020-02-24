cbd9d9e29f63e370d14a62e1dfa74a59571589cc
toshinari-ishii <toshi1225m@gmail.com>
2020-02-24 17:57:19 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @items = Item.where(user_id: @user.id)
-    @photos = Photo.all
+    @item = Item.find(params[:id])
+    @photos = @item.photos
   end
 end
